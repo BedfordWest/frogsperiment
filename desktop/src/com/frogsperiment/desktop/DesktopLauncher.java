4863ed06547ea85fe772db43531c127044796985
bedfordwest <bedfordwest@gmail.com>
2015-09-02 22:36:14 -0500

    feat(skeleton): creation of the initial skeleton structure
    does not currently compile - will fix soon.

--- desktop/src/com/frogsperiment/desktop/DesktopLauncher.java
+++ desktop/src/com/frogsperiment/desktop/DesktopLauncher.java
@@ -2,11 +2,35 @@ package com.frogsperiment.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.frogsperiment.FrogsperimentGame;
+import com.frogsperiment.util.Constants;
 
 public class DesktopLauncher {
+
+    private static boolean rebuildAtlas = false;
+    private static boolean drawDebugOutline = false;
+
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        if (rebuildAtlas) {
+            Settings settings = new Settings();
+            settings.maxWidth = 1024;
+            settings.maxHeight = 1024;
+            settings.duplicatePadding = true;
+            settings.debug = drawDebugOutline;
+            TexturePacker.process(settings, "android/assets-raw/images",
+                    "android/assets/images", "worldtextures");
+        }
+
+		LwjglApplicationConfiguration config =
+                new LwjglApplicationConfiguration();
+
+        config.title = "Frogsperiment Development Build v0.1d";
+        config.width = Constants.DESKTOP_WIDTH;
+        config.height = Constants.DESKTOP_HEIGHT;
+
 		new LwjglApplication(new FrogsperimentGame(), config);
 	}
 }
