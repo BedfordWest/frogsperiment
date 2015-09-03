d18bd165153ad30e0eafda877919188cad8ccfa1
bedfordwest <bedfordwest@gmail.com>
2015-09-02 20:59:53 -0500

    feat(initial_commit): Initial commit of a blank LibGDX project.

---
+++ desktop/src/com/frogsperiment/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.frogsperiment.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.frogsperiment.FrogsperimentGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FrogsperimentGame(), config);
+	}
+}
